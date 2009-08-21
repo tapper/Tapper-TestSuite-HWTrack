4ddc2afffcc9095d33b4d29c4db51a81b73c1a90
Maik Hentsche <maik.hentsche@amd.com>
2009-08-21 13:07:26 +0200

    HWTrack: added scripts for dist uploads

---
+++ scripts/dist_upload_wotan.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+EXECDIR=$(dirname $0)
+DISTFILES='TestSuite*-*.*.tar.gz '
+$EXECDIR/artemis_version_increment.pl $EXECDIR/../lib/TestSuite/HWTrack.pm
+cd $EXECDIR/..
+
+rm MANIFEST
+make manifest || exit -1
+
+perl Makefile.PL || exit -1
+make dist || exit -1
+
+# -----------------------------------------------------------------
+# It is important to not overwrite existing files.
+# -----------------------------------------------------------------
+# That guarantees that the version number is incremented so that we
+# can be sure about version vs. functionality.
+# -----------------------------------------------------------------
+
+echo ""
+echo '----- upload ---------------------------------------------------'
+rsync -vv --progress --ignore-existing ${DISTFILES} artemis@wotan:/home/artemis/CPANSITE/CPAN/authors/id/A/AR/ARTEMIS/
+
+echo ""
+echo '----- re-index -------------------------------------------------'
+ssh artemis@wotan /home/artemis/perl510/bin/cpansite -vl index /home/artemis/CPANSITE/CPAN/
+ssh artemis@wotan /home/artemis/perl510/bin/cpansite install TestSuite::HWTrack
+
