28ae932bdbbd166b62e501995960fc3d16c33a67
Maik Hentsche <maik.hentsche@amd.com>
2009-09-04 10:15:38 +0200

    HWTrack: version++

--- lib/TestSuite/HWTrack.pm
+++ lib/TestSuite/HWTrack.pm
@@ -25,7 +25,7 @@ TestSuite::HWTrack - Keep track of actual hardware in test machine
 =cut
 
         package TestSuite::HWTrack;
-        our $VERSION = '0.010013';
+        our $VERSION = '0.010014';
 
 =head1 SYNOPSIS
 
